83be8b29b207e799ee2e7212d1700e7cb6c52626
jones47bj <144172259+jones47bj@users.noreply.github.com>
2023-09-17 19:36:24 -0400

    Update search_eval.py

--- search_eval.py
+++ search_eval.py
@@ -21,7 +21,7 @@ class InL2Ranker(metapy.index.RankingFunction):
         @see https://meta-toolkit.org/doxygen/structmeta_1_1index_1_1score__data.html
         """
         #tfn=1
-        tfn = sd.doc_term_count * math.log((1+(sd.avg_dl/abs(sd.d_id))),2)
+        tfn = sd.doc_term_count * math.log((1+(sd.avg_dl/abs(sd.doc_size))),2)
         #(sd.doc_count * math.log((1+(sd.avg_dl/document)),2))
         score = sd.query_term_weight * (tfn/(tfn+self.param)) * math.log(((sd.num_docs+1)/(sd.corpus_term_count + .5)),2)
         return score
