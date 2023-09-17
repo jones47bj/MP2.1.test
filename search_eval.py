48343c54c79e206555f594094a88eb7fc3065b03
jones47bj <144172259+jones47bj@users.noreply.github.com>
2023-09-17 19:20:43 -0400

    Update search_eval.py

--- search_eval.py
+++ search_eval.py
@@ -20,9 +20,9 @@ class InL2Ranker(metapy.index.RankingFunction):
         For fields available in the score_data sd object,
         @see https://meta-toolkit.org/doxygen/structmeta_1_1index_1_1score__data.html
         """
-        tfn = sd.doc_count * math.log((1+(sd.avg_dl/sd.d_id)),2)
+        #tfn = sd.doc_count * math.log((1+(sd.avg_dl/sd.d_id)),2)
         #(sd.doc_count * math.log((1+(sd.avg_dl/document)),2))
-        #score = sd.query_term_weight * (tfn/(tfn+self.param)) * math.log(((sd.num_docs+1)/(sd.corpus_term_count + .5)),2)
+        score = sd.query_term_weight * (tfn/(tfn+self.param)) * math.log(((sd.num_docs+1)/(sd.corpus_term_count + .5)),2)
         return 0
         #return (self.param + sd.doc_term_count) / (self.param * sd.doc_unique_terms + sd.doc_size)
 
